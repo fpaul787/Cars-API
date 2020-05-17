b06e1091dd0d328cfa50eea594c8f284447aa209
Frantz <fpaul787@gmail.com>
2020-05-17 10:04:03 -0400

    removing comment.

--- src/main/java/com/truckla/cars/CarsApplication.java
+++ src/main/java/com/truckla/cars/CarsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarsApplication {
 
-	//COMMITING THIS
+
 	public static void main(String[] args) {
 		SpringApplication.run(CarsApplication.class, args);
 	}
